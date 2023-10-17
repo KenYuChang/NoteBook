e49f7342a8b56cc28949a6db09d7686d9382d568
KenYuChang <justin0915justin0915@gmail.com>
2023-10-17 21:59:15 +0800

    noteController

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,22 +1,38 @@
 const asyncHandler = require('../middleware/asyncHandler');
-const Notes = require('../models/notes');
+const { Notes } = require('../models');
 
 const noteConroller = {
-  getAllNotes: (req, res) => {
-    return res.send('get all notes');
-  },
-  getNote: (req, res) => {
-    return res.send('get note');
-  },
-  createNote: (req, res) => {
+  getAllNotes: asyncHandler(async (req, res) => {
+    const notes = await Notes.findAll({
+      raw: true,
+      nest: true,
+    });
+
+    res.json(notes);
+  }),
+  getNote: asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const note = await Notes.findByPk(id, {
+      raw: true,
+      nest: true,
+    });
+
+    if (!note) {
+      res.status(404);
+      throw new Error('Resource not found');
+    }
+
+    res.json(note);
+  }),
+  createNote: asyncHandler(async (req, res) => {
     return res.send('create note');
-  },
-  editNote: (req, res) => {
+  }),
+  editNote: asyncHandler(async (req, res) => {
     return res.send('edit note');
-  },
-  deleteNote: (req, res) => {
+  }),
+  deleteNote: asyncHandler(async (req, res) => {
     return res.send('delete note');
-  },
+  }),
 };
 
 module.exports = noteConroller;
